1863aa96dd4950e97e52dcc21e05658baccfb724
angela.brunst <angela.hairstylist@gmail.com>
2023-01-09 22:01:05 -0700

    Finished the Basics tutorial

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -4,11 +4,33 @@ import { Component } from '@angular/core';
   selector: 'app-servers',
   // selector: '[app-servers]',
   // selector: '.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'Testserver';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
+
+  constructor() {
+    setTimeout(()=> {
+      this.allowNewServer = true;
+    }, 2000)
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created. Name is ' + this.serverName + ".";
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 
 }
