6bc88aaf5b82ec6db8ff0e6354b59b39f3fe9303
angela.brunst <angela.hairstylist@gmail.com>
2023-01-13 22:10:41 -0700

    Created cms project

---
+++ cms/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactDetailComponent } from './contacts/contact-detail/contact-detail.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactDetailComponent,
+    ContactListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
