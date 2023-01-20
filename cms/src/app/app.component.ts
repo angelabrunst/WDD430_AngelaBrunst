ec8dbdf6827bb956de0f0853b62b461cc4a7932a
angela.brunst <angela.hairstylist@gmail.com>
2023-01-19 22:32:35 -0700

    Header working

--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  selectedFeature: string = 'documents';
+
+  switchView(feature: string) {
+    this.selectedFeature = feature;
+  }
 }
