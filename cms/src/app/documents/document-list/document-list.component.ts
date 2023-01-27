21eb7c2c9cd0eb3456f3756e5495818c1fd56076
angela.brunst <angela.hairstylist@gmail.com>
2023-01-26 19:02:32 -0700

    finished week 4

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Document } from '../document.model';
 
 @Component({
   selector: 'cms-document-list',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./document-list.component.css']
 })
 export class DocumentListComponent {
+  @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
+  documents: Document[] = [
+    new Document(1, 'Week 4 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
+    new Document(1, 'Week 5 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
+    new Document(1, 'Week 6 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
+    new Document(1, 'Week 7 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
+    new Document(1, 'Week 8 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
+  ];
+
+  onSelectedDocument(document: Document) {
+    this.selectedDocumentEvent.emit(document);
+    console.log("this works");
+  }
 }
