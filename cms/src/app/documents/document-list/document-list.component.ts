ea94fdec0d254b188ac9a92fda7a1b746ccc066e
angela.brunst <angela.hairstylist@gmail.com>
2023-02-02 21:06:30 -0700

    Implement Document Service

--- cms/src/app/documents/document-list/document-list.component.ts
+++ cms/src/app/documents/document-list/document-list.component.ts
@@ -1,24 +1,24 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Document } from '../document.model';
+import { DocumentService } from '../document.service';
 
 @Component({
   selector: 'cms-document-list',
   templateUrl: './document-list.component.html',
   styleUrls: ['./document-list.component.css']
 })
-export class DocumentListComponent {
+export class DocumentListComponent implements OnInit{
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
 
-  documents: Document[] = [
-    new Document(1, 'Week 4 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
-    new Document(1, 'Week 5 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
-    new Document(1, 'Week 6 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
-    new Document(1, 'Week 7 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
-    new Document(1, 'Week 8 Assignment', 'Learning many new things that are important.', 'https://my.byui.edu/ICS/', null),
-  ];
+  documents: Document[] = [];
+
+  constructor(private documentService: DocumentService) {}
+
+  ngOnInit() {
+    this.documents = this.documentService.getDocuments(); 
+  }
 
   onSelectedDocument(document: Document) {
     this.selectedDocumentEvent.emit(document);
-    console.log("this works");
   }
 }
---
+++ cms/src/app/documents/document.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Document } from './document.model';
+import { MOCKDOCUMENTS } from './MOCKDOCUMENTS';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DocumentService {
+
+  documents: Document[] = [];
+
+  constructor() {
+    this.documents = MOCKDOCUMENTS;
+  }
+
+  getDocuments() {
+    return this.documents.slice();
+  }
+
+  getDocument(id: string): Document {
+    if (!this.documents) {
+      return null;
+    }
+
+    for (let document of this.documents) {
+      if (document.id === id) {
+        return document;
+      }
+    }
+
+    return null;
+  }
+}
