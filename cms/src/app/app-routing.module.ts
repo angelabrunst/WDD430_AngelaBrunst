2a84ce90efe13b40c2f34b06cbe84c7843829d12
angela.brunst <angela.hairstylist@gmail.com>
2023-02-09 20:38:38 -0700

    Create routing for main components

--- cms/src/app/app-routing.module.ts
+++ cms/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './contacts/contacts.component';
+import { DocumentsComponent } from './documents/documents.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
 
-const routes: Routes = [];
+const AppRoutes: Routes = [
+  {path: '', redirectTo: '/documents', pathMatch: 'full'},
+  {path: 'documents', component: DocumentsComponent},
+  {path: 'messages', component: MessageListComponent},
+  {path: 'contacts', component: ContactsComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(AppRoutes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- cms/src/app/app.component.ts
+++ cms/src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  selectedFeature: string = 'documents';
-
-  switchView(feature: string) {
-    this.selectedFeature = feature;
-  }
+  title = 'We learn CMS!';
 }
