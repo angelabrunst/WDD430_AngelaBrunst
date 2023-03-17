5a2df15bf97901e79192e0f4ef4d4b9b8c6e5cbe
angela.brunst <angela.hairstylist@gmail.com>
2023-03-16 20:45:41 -0600

    Updated Routing files

--- cms/server/routes/contacts.js
+++ cms/server/routes/contacts.js
@@ -2,10 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 const sequenceGenerator = require('./sequenceGenerator');
-const Document = require('../models/document');
+const Contact = require('../models/contact');
 
 router.get('/', (req, res, next) => {
-    Document.find()
+    Contact.find()
+    .populate('group')
     .exec(function(err, result) {
         if (err) {
             return res.status(500).json({
@@ -15,85 +16,87 @@ router.get('/', (req, res, next) => {
         }
         return res.status(200).json(result);
     });
-
 });
 
 router.post('/', (req, res, next) => {
-    const maxDocumentId = sequenceGenerator.nextId("documents");
+    const maxDocumentId = sequenceGenerator.nextId("contacts");
 
-    const document = new Document({
+    const contact = new Contact({
         id: maxDocumentId,
         name: req.body.name,
-        description: req.body.description,
-        url: req.body.url
+        email: req.body.email,
+        phone: req.body.phone,
+        imageUrl: req.body.imageUrl,
+        group: []
     });
 
-    document.save()
-        .then(createdDocument => {
+    contact.save()
+        .then(createdContact => {
             res.status(201).json({
-                message: 'Document added successfully',
-                document: createdDocument
+                message: 'Contact added successfully',
+                contact: createdContact
             });
         })
         .catch(error => {
             res.status(500).json({
-            message: 'An error occurred',
+            message: 'Error creating contact',
             error: error
             });
         });
 });
 
 router.put('/:id', (req, res, next) => {
-    Document.findOne({ id: req.params.id })
-      .then(document => {
-        document.name = req.body.name;
-        document.description = req.body.description;
-        document.url = req.body.url;
+    Contact.findOne({ id: req.params.id })
+      .then(contact => {
+        contact.name = req.body.name,
+        contact.email = req.body.email,
+        contact.phone = req.body.phone,
+        contact.imageUrl =req.body.imageUrl,
   
-        Document.updateOne({ id: req.params.id }, document)
+        Contact.updateOne({ id: req.params.id }, contact)
           .then(result => {
             res.status(204).json({
-              message: 'Document updated successfully'
+              message: 'Contact updated successfully'
             })
           })
           .catch(error => {
              res.status(500).json({
-             message: 'An error occurred',
+             message: 'An error occurred while updating contact',
              error: error
            });
           });
       })
       .catch(error => {
         res.status(500).json({
-          message: 'Document not found.',
-          error: { document: 'Document not found'}
+          message: 'Contact not found.',
+          error: { contact: 'Contact not found'}
         });
       });
 });
 
 router.delete("/:id", (req, res, next) => {
-    Document.findOne({ id: req.params.id })
-      .then(document => {
-        Document.deleteOne({ id: req.params.id })
+    Contact.findOne({ id: req.params.id })
+      .then(contact => {
+        Contact.deleteOne({ id: req.params.id })
           .then(result => {
             res.status(204).json({
-              message: "Document deleted successfully"
+              message: "Contact deleted successfully"
             });
           })
           .catch(error => {
              res.status(500).json({
-             message: 'An error occurred',
+             message: 'An error occurred while deleting contact',
              error: error
            });
           })
       })
       .catch(error => {
         res.status(500).json({
-          message: 'Document not found.',
-          error: { document: 'Document not found'}
+          message: 'Contact not found.',
+          error: { contact: 'Contact not found'}
         });
       });
   });
 
 
-module.exports = router; 
+module.exports = router;
